dacb301e5b096f37b3df38c61ed6d15d1e38e3d5
Bhaskar Bahukhandi <95625149+pokegithub@users.noreply.github.com>
2026-04-27 03:17:19 +0530

    Add files via upload

---
+++ data_vectorizer.py
@@ -0,0 +1,30 @@
+import pandas as pd
+from sklearn.feature_extraction.text import TfidfVectorizer
+from tqdm import tqdm
+import joblib
+
+df = pd.read_csv('preprocessed_training_data.csv')
+df1 = pd.read_csv('preprocessed_testing_data.csv')
+
+x = df['clean_text']
+x1 = df1['clean_text']
+y = df['label']
+y1 = df1['label']
+
+print(x.value_counts().sum(), x1.value_counts().sum())
+
+tfidf = TfidfVectorizer(
+    max_features=5000,  
+    min_df=10,          
+    ngram_range=(1, 1)  # (1,1) -> single words
+)
+
+x_vec = tfidf.fit_transform(tqdm(x, desc = 'Training: TF-IDF Vectorization'))
+x1_vec = tfidf.transform(tqdm(x1, desc = 'Testing: TF-IDF Vectorization'))
+
+joblib.dump(x_vec, 'vectors/x_training_vector.pkl')
+joblib.dump(x1_vec, 'vectors/x_testing_vector.pkl')
+joblib.dump(y, 'vectors/y_training_vector.pkl')
+joblib.dump(y1, 'vectors/y_testing_vector.pkl')
+joblib.dump(tfidf, 'vectors/tfidf_vectorizer.pkl')
+
